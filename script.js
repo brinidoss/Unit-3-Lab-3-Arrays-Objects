d51011a2d72c233714f52ddd3a1054bd303f96b6
Brittany Doss <brinidoss@gmail.com>
2021-04-29 22:00:31 -0400

    half done

--- script.js
+++ script.js
@@ -43,9 +43,9 @@ function addSubmission(array, newName, newScore, newDate) {
 }
 
 
-addSubmission(submissions, 'Judy', 40, '2019-05-17');
+//addSubmission(submissions, 'Judy', 40, '2019-05-17');
 
-console.log(submissions);
+//console.log(submissions);
 
 //Delete by Index
 
@@ -54,13 +54,72 @@ function deleteSubmissionByIndex(array, index) {
     array.splice(index, 1)
 }
 
-deleteSubmissionByIndex(submissions, 2);
+//deleteSubmissionByIndex(submissions, 2);
 
-console.log(submissions);
+//console.log(submissions);
 
 //delete by Name
-/*
+
 function deleteSubmissionByName(array, name) {
+ 
+    const nameIndex = array.findIndex((item)=> item.name === name); 
+        return array.splice(nameIndex, 1);
     
-    array.findIndex()
-} */
+} 
+
+//deleteSubmissionByName(submissions, 'Joe');
+//console.log(submissions);
+
+//edit submission 
+
+// function editSubmission(array, index, score) {
+//     const scoreIndex = array.findIndex((item) => item.score === index); 
+//     //return scoreIndex;
+//     return array.splice(scoreIndex, 1, score);
+//     score >= 60 ? passed = true : passed = false;
+// }
+
+// editSubmission(submissions, 2, 70);
+
+// console.log(submissions);
+
+function editSubmission(array, index, newScore) {
+    array[index].score = newScore;
+    newScore >= 60 ? array[index].passed = true : array[index].passed = false;
+}
+//console.log(submissions);
+//editSubmission(submissions, 1, 40);
+//console.log(submissions);
+
+// function editSubmission(array, index, newScore) {
+//     if (i === index) {
+
+//     }
+// }
+
+// function editSubmission()
+// array.forEach(function(submission, i) {
+//     if (i === index) {
+//         updatedSubmission = submission
+//     }
+// })
+
+function findSubmissionByName(array,name) {
+
+}
+
+function findLowestScore(array) {
+
+}
+
+function findAverageScore(array) {
+
+}
+
+function filterPassing(array) {
+
+}
+
+function filter90AndAbove(array) {
+
+}
